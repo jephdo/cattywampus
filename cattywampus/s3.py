17a53e9867df4a586d31fedb24771a06bc8d95b5
Jeph Do <jephdo@gmail.com>
2016-03-13 13:26:22 -0700

    typo

--- cattywampus/s3.py
+++ cattywampus/s3.py
@@ -26,7 +26,7 @@ def get_client():
 def file_exists(s3path):
     try:
         bucket, key = bucket_and_key_from_path(s3path)
-    except KeyError:
+    except ValueError:
         return False
     client = get_client()
     try:
